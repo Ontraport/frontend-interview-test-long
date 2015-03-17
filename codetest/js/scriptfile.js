c7ca39bbf5ec124404ba3cd5d4c451348a605e1e
Andy Barajas <andyb@procore.com>
2015-03-16 21:04:07 -0700

    Added profile img to navbar, to user profile column, white shadow for search bar

--- codetest/js/scriptfile.js
+++ codetest/js/scriptfile.js
@@ -26,3 +26,9 @@ $(document).ready(function() {
          }
     });
 });
+
+$.getJSON( "data/users.json", function(data) {
+    //for (var i=0, len=data.length; i < len; i++) {
+        console.log(data[i].id);
+    //}
+});
