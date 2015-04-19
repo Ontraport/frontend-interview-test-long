aff240ec8b1614e8e4da34f477c867a696060696
Kyle Espinola <ksespinola@gmail.com>
2015-04-19 10:20:43 -0700

    add catchall to router

--- codetest/src/scripts/components/routes.jsx
+++ codetest/src/scripts/components/routes.jsx
@@ -6,10 +6,9 @@ var NotFoundRoute       = Router.NotFoundRoute;
 var Redirect            = Router.Redirect;
 
 module.exports = (
-    <Route name="home" handler={require('./app.jsx')} path="/codetest/" >
-      <Redirect path="/codetest" to="home" />
-      <Route name="profile" handler={require('./profile.jsx')} path="profile/:userId" />
-      <DefaultRoute handler={require('./profile.jsx')} />
+    <Route name="home" handler={require('./app.jsx')} path="/">
+      <Route name="profile" handler={require('./profile.jsx')} path="/profile/:userId" />
+      <Route handler={require('./profile.jsx')} path="*" />
       <NotFoundRoute handler={require('./not-found.jsx')} />
     </Route>
 );
