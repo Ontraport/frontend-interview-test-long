ed649af1602e4c2ad7b2571b4e3e899dbe70eb9f
John Pittman <johnrichadrpittman@gmail.com>
2015-04-23 00:48:22 -0400

    Added throw errors on unimplemented methods.

--- codetest-famous-3.5/src/services/PostsService.js
+++ codetest-famous-3.5/src/services/PostsService.js
@@ -5,6 +5,18 @@ function PostsService() {
     this.fetchAllPosts = function() {
         return http.get('_data/posts.json');
     };
+
+    this.addPost = function(post) {
+        throw new Error('Not implemented.');
+    };
+
+    this.removePost = function(postId) {
+        throw new Error('Not implemented.');
+    };
+
+    this.addComment = function(postId, comment) {
+        throw new Error('Not implemented.');
+    };
 }
 
 module.exports = new PostsService();
