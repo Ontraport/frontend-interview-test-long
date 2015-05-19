d470c8fc1064686ea5b9530410da63be50ce5104
sidho <whoissidho@gmail.com>
2015-05-18 19:52:15 -0700

    Remove close button from modal

--- codetest/javascripts/modal.js
+++ codetest/javascripts/modal.js
@@ -2,7 +2,8 @@ $(function() {
   $("#post-link").click(function(event) {
     $(this).modal({
       fadeDuration: 250,
-      opacity: .50
+      opacity: .50,
+      showClose: false
     });
     return false;
   });
