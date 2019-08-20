f3b2c805b972cb59c8401d78bd6b5a9140574f51
Jared Seeders <jared.seeders@gmail.com>
2019-08-20 14:03:25 -0700

    added sort by option

--- codetest/dist/bundle.js
+++ codetest/dist/bundle.js
@@ -1028,6 +1028,13 @@ class PostsPage extends _base_component_js__WEBPACK_IMPORTED_MODULE_0__["BaseCom
 				<div id="postPanel" class="panel">				
 					<div class="panel-content">					
 						<h1>Updates</h1>
+						<div id='sortByWrapper'>
+							sort by 
+							<select id='sortBy'>
+								<option value='old'>old</option>
+								<option value='new'>new</option>
+							</select>
+						</div>
 						<div class="post-list">
 							${this.posts}
 						</div>
@@ -1053,6 +1060,15 @@ class PostsPage extends _base_component_js__WEBPACK_IMPORTED_MODULE_0__["BaseCom
 
 				#wrapper h1 {
 					margin-bottom: 28px;
+					float: left;
+				}
+				
+				#sortByWrapper {
+					float: right;
+				}
+				
+				.post-list {
+					clear: both;
 				}
 
 			</style>`;
@@ -1062,17 +1078,31 @@ class PostsPage extends _base_component_js__WEBPACK_IMPORTED_MODULE_0__["BaseCom
 		this.user_id = this.getAttribute("data-user-id");
 		this.user_pic = this.getAttribute("data-user-pic");
 		this.user_name = this.getAttribute("data-user-name");		
+		this.sortBy = null;
 	}
 	
 	Render(){
 		this.RenderPosts();
 		this.shadowRoot.innerHTML = this.style + this.html;		
+		
 	}
 	
 	AttachEvents(){		
+		var $this = this;
+		this.sortBy = this.shadowRoot.querySelector("#sortBy");
+		this.sortBy.addEventListener("change", function(e){			
+			$this.RenderPosts();
+			$this.postContainer.innerHTML = $this.posts;
+			$this.SetPostArray();
+		});
+		
 		this.postContainer = this.shadowRoot.querySelector("#postPanel .post-list");
-		var renderedPosts = this.postContainer.querySelectorAll("user-post");
-		console.log(renderedPosts);
+		this.SetPostArray();
+	}
+	
+	//store posts for later access
+	SetPostArray(){
+		let renderedPosts = this.postContainer.querySelectorAll("user-post");
 		for(let post of renderedPosts){
 			this.postArray[post.post_id] = post;
 		}
@@ -1092,8 +1122,13 @@ class PostsPage extends _base_component_js__WEBPACK_IMPORTED_MODULE_0__["BaseCom
 					newPost.setAttribute("data-post-id", postData.id);
 					newPost.setAttribute("data-user-name", userData.username);
 					newPost.setAttribute("data-user-pic", userData.pic);
-					newPost.innerHTML = postData.content;					
-					this.posts = this.posts + newPost.outerHTML;
+					newPost.innerHTML = postData.content;	
+						
+					if(this.sortBy == null || this.sortBy.value == 'old'){
+						this.posts = this.posts + newPost.outerHTML;
+					} else {
+						this.posts = newPost.outerHTML + this.posts;
+					}
 				}
 			}	
 		}			
--- codetest/src/components/posts-page.js
+++ codetest/src/components/posts-page.js
@@ -14,6 +14,13 @@ export class PostsPage extends BaseComponent {
 				<div id="postPanel" class="panel">				
 					<div class="panel-content">					
 						<h1>Updates</h1>
+						<div id='sortByWrapper'>
+							sort by 
+							<select id='sortBy'>
+								<option value='old'>old</option>
+								<option value='new'>new</option>
+							</select>
+						</div>
 						<div class="post-list">
 							${this.posts}
 						</div>
@@ -39,6 +46,15 @@ export class PostsPage extends BaseComponent {
 
 				#wrapper h1 {
 					margin-bottom: 28px;
+					float: left;
+				}
+				
+				#sortByWrapper {
+					float: right;
+				}
+				
+				.post-list {
+					clear: both;
 				}
 
 			</style>`;
@@ -48,17 +64,31 @@ export class PostsPage extends BaseComponent {
 		this.user_id = this.getAttribute("data-user-id");
 		this.user_pic = this.getAttribute("data-user-pic");
 		this.user_name = this.getAttribute("data-user-name");		
+		this.sortBy = null;
 	}
 	
 	Render(){
 		this.RenderPosts();
 		this.shadowRoot.innerHTML = this.style + this.html;		
+		
 	}
 	
 	AttachEvents(){		
+		var $this = this;
+		this.sortBy = this.shadowRoot.querySelector("#sortBy");
+		this.sortBy.addEventListener("change", function(e){			
+			$this.RenderPosts();
+			$this.postContainer.innerHTML = $this.posts;
+			$this.SetPostArray();
+		});
+		
 		this.postContainer = this.shadowRoot.querySelector("#postPanel .post-list");
-		var renderedPosts = this.postContainer.querySelectorAll("user-post");
-		console.log(renderedPosts);
+		this.SetPostArray();
+	}
+	
+	//store posts for later access
+	SetPostArray(){
+		let renderedPosts = this.postContainer.querySelectorAll("user-post");
 		for(let post of renderedPosts){
 			this.postArray[post.post_id] = post;
 		}
@@ -78,8 +108,13 @@ export class PostsPage extends BaseComponent {
 					newPost.setAttribute("data-post-id", postData.id);
 					newPost.setAttribute("data-user-name", userData.username);
 					newPost.setAttribute("data-user-pic", userData.pic);
-					newPost.innerHTML = postData.content;					
-					this.posts = this.posts + newPost.outerHTML;
+					newPost.innerHTML = postData.content;	
+						
+					if(this.sortBy == null || this.sortBy.value == 'old'){
+						this.posts = this.posts + newPost.outerHTML;
+					} else {
+						this.posts = newPost.outerHTML + this.posts;
+					}
 				}
 			}	
 		}			
