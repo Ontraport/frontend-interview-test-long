1266716f3406b9a23089a79eea60d28ae598e009
Andrew Westwick <awestwick@gmail.com>
2020-01-25 12:17:42 -0600

    add basic layout for profile and posts

--- codetest-angular/src/app/app.module.ts
+++ codetest-angular/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule
